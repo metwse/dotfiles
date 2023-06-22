b7792e5511fe88d9c2e1019981484d7f6471aaf6
Metehan Selvi <me@metehanselvi.com>
2023-06-23 00:23:48 +0300

    1.4.0

--- .config/i3/popup.sh
+++ .config/i3/popup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-id=$(i3-msg -t subscribe -m '[ "window" ]' | grep -m 1 -oP "\"change\":\"new\",\"container\":.*\"id\":\K(\d*)(?=.*\"window_role\":\"pop\-up\".*)")
-echo $id
-i3-msg "[con_id=$id] floating enable; resize set width 720 px height 640 px; move position center; border none"
+while :; do
+    id=$(i3-msg -t subscribe -m '[ "window" ]' | grep -m 1 -oP "\"change\":\"new\",\"container\":.*\"id\":\K(\d*)(?=.*\"window_role\":\"pop\-up\".*)")
+    if [[ "$?" = 0 ]]; then
+        i3-msg "[con_id=$id] floating enable; resize set width 720 px height 640 px; move position center; border none"
+    else
+        sleep 1
+    fi
+done
