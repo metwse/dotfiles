47ebfdd03be6288e4422794d17e565c38d070b1e
Metehan Selvi <me@metehanselvi.com>
2023-11-06 00:04:05 +0300

    1.10.6

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ sudo apt install apt-transport-https --yes
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo sh -c "echo 'deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/jammy pgadmin4 main' >> /etc/apt/sources.list.d/pgadmin4.list"
 sudo sh -c "echo 'deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x jammy main\ndeb-src [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x jammy main' >> /etc/apt/sources.list.d/node.list"
-sudo sh -c "echo 'deb https://baltocdn.com/i3-window-manager/i3/i3-autobuild/ all main' >> /etc/apt/sources.list.d/i3.list"
+sudo sh -c "echo 'deb https://baltocdn.com/i3-window-manager/i3/i3-autobuild-ubuntu/ all main' >> /etc/apt/sources.list.d/i3.list"
 
 sudo apt update
 
-sudo apt install lxpolkit picom i3 maim neovim xclip feh git tmux golang nodejs wget rofi i3blocks xbacklight surf golang sway swayidle
+sudo apt install lxpolkit picom i3 maim neovim xclip feh git tmux golang nodejs wget rofi i3blocks xbacklight surf golang sway swayidle xss-lock
 sudo npm install -g tree-sitter-cli
 
 
