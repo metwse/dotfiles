6d6477558013d1cd1243240cf6f34a3a165b8e80
metwse <108795071+metwse@users.noreply.github.com>
2025-03-25 14:41:27 +0300

    nvim: listchars / i3: i3blocks

--- config/nvim/lua/metw/set.lua
+++ config/nvim/lua/metw/set.lua
@@ -2,7 +2,7 @@ vim.opt.nu = true
 vim.opt.rnu = true
 
 vim.opt.smartindent = true
-vim.opt.wrap = false 
+vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backupdir = os.getenv('HOME') .. '/.vim/backupdir'
@@ -22,5 +22,27 @@ vim.g.mapleader = ' '
 
 vim.opt.fdm = 'marker'
 vim.opt.colorcolumn = '80'
+
 pcall(vim.cmd, { 'au BufWinLeave * mkview' })
 pcall(vim.cmd, { 'au BufWinEnter * silent loadview' })
+
+
+vim.opt.list = true
+
+vim.opt.showbreak = '↪'
+vim.opt.listchars = 'tab:│ ,trail:█,extends:…,nbsp:‿'
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+    callback = function()
+        vim.api.nvim_set_hl(0, "TrailingWhitespace", { link = "Whitespace" })
+    end
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+    callback = function()
+        vim.api.nvim_set_hl(0, "TrailingWhitespace", { link = "Error" })
+    end
+})
+
+vim.cmd([[match TrailingWhitespace /\s\+$/]])
+vim.api.nvim_set_hl(0, "TrailingWhitespace", { link = "Error" })
