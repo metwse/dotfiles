4ae16d8eab88d485e34fdc4598ac0ee91431b59f
Metehan Selvi <me@metehanselvi.com>
2025-12-28 00:16:28 +0300

    refactor installation script

--- install.sh
+++ install.sh
@@ -1,25 +1,41 @@
-# adding repos
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo add-apt-repository -y ppa:neovim-ppa/unstable  # Daily builds of nvim
+
 sudo apt update
 
-sudo apt install -y\
-    curl apt-transport-https wget\
-    lxpolkit picom i3 maim xclip feh rofi i3blocks xbacklight xss-lock\
-    golang nodejs python3-venv\
-    neovim git tmux\
-    ripgrep todotxt-cli\
-    sway swaylock swayidle waybar\
-    foot\
-    wl-clipboard cliphist\
-    grim slurp\
-    jq
-sudo npm install -g tree-sitter-cli
+packages=(
+    apt-transport-https wget curl  # networking
+    i3 lxpolkit picom xbacklight xss-lock  # window manager (X11)
+    sway swaylock swayidle waybar  # window manager (Wayland)
+    maim xclip feh i3blocks  # X11 utilities
+    wl-clipboard cliphist grim slurp  # Wayland utilities
+    foot  # Wayland terminal
+    neovim git tmux  # core dev tools
+    ripgrep todotxt-cli  # CLI tools
+    golang rustup python3-venv  # languages
+    jq yq  # JSON/YAML utilities
+    clang clangd  # C
+    manpages manpages-dev manpages-posix manpages-posix-dev  # manpages
+)
+
+sudo apt install -y "${packages[@]}"
 
 
 # link config
 ln -sf ~/.w/config/* ~/.config
 ln -sf ~/.w/tmux.conf ~/.tmux.conf
 ln -sf ~/.w/bashrc ~/.bashrc
+
+
+# install node & npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install --lts
+nvm use --lts
+
+npm install -g tree-sitter-cli
 
 
 # fonts
@@ -41,16 +57,19 @@ fc-cache -vf ~/.fonts
 
 
 # rofi-tdk
-sudo wget https://github.com/metwse/rofi-tdk.sh/releases/latest/download/rofi-tdk.tar.gz -O /var/rofi-tdk.tar.gz
+sudo wget \
+    https://github.com/metwse/rofi-tdk.sh/releases/latest/download/rofi-tdk.tar.gz \
+    -O /var/rofi-tdk.tar.gz
 
 
 # tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 
-# greenclip -- commented out since i switched to wayland
-# sudo wget https://github.com/erebe/greenclip/releases/download/v4.2/greenclip -O /usr/bin/greenclip
-# sudo chmod +x /usr/bin/greenclip
+# greenclip
+sudo wget https://github.com/erebe/greenclip/releases/download/v4.2/greenclip \
+    -O /usr/bin/greenclip
+sudo chmod +x /usr/bin/greenclip
 
 
 # keyboard layout
